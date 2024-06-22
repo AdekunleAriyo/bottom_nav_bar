3020dfc0a6d34108d6224fee11c944e2015b57dd
AdekunleAriyo <dekunlearry@gmail.com>
2024-06-22 18:56:02 +0100

    Simple bottom navigation bar

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,52 @@
+import 'package:bottom_nav_bar/pages/bar_item_page.dart';
+import 'package:bottom_nav_bar/pages/home_page.dart';
+import 'package:bottom_nav_bar/pages/my_page.dart';
+import 'package:bottom_nav_bar/pages/search_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        // backgroundColor: Colors.red,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Bar'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'My'),
+        ],
+      ),
+    );
+  }
+}
